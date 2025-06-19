bb9677eaabc808ee15e60bc6a9cff49681b09ec5
tsctsc6 <tsctsc6@126.com>
2025-06-19 17:26:41 +0800

    更新 main 函数

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,13 @@
 mod qb_sdk;
+mod command;
+mod log;
 
 use clap::Parser;
-use reqwest::Error;
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
-}
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
-    }
-    
-    let qb_client = qb_sdk::QbClient::new();
-    qb_client.test().await?;
+async fn main() -> Result<(), String> {
+    let cli = command::Cli::parse();
+    let qb_client = qb_sdk::QbClient::new(cli);
+    qb_client.ensure_api_version().await?;
     Ok(())
 }
