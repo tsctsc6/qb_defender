d987281ae4a96e61ec1a5b504126e60ab06ad025
tsctsc6 <tsctsc6@126.com>
2025-06-21 13:44:25 +0800

    remove debug message

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,6 @@ async fn run() -> Result<(), String> {
     qb_client.ensure_api_version().await?;
     qb_client.reset_banned_IPs().await?;
     loop {
-        println!("Waiting...");
         qb_client.try_reset_banned_IPs().await?;
         qb_client.record_and_ban_peers().await?;
         qb_client.wait().await;
