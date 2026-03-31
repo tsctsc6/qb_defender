8f239d6bb728725796bbd1f199d2ae428436d7f4
tsctsc6 <tsctsc6@126.com>
2026-03-31 16:11:09 +0800

    use tracing for log

---
+++ src/logger.rs
@@ -0,0 +1,56 @@
+use thiserror::Error;
+use time::macros::format_description;
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_appender::rolling::{RollingFileAppender, Rotation};
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Failed to initialize logger: {0}")]
+    SetLoggerError(#[from] log::SetLoggerError),
+
+    #[error("Rolling file error: {0}")]
+    RollingFileError(#[from] tracing_appender::rolling::InitError),
+
+    #[error("IO error: {0}")]
+    IOError(#[from] std::io::Error),
+}
+
+pub fn init_logger() -> Result<WorkerGuard, Error> {
+    let file_appender = RollingFileAppender::builder()
+        .rotation(Rotation::DAILY)
+        .filename_prefix("app")
+        .filename_suffix("log")
+        .max_log_files(7)
+        .build("logs")?;
+
+    // guard must keep alive to ensure logs are flushed properly
+    let (non_blocking_appender, guard) = tracing_appender::non_blocking(file_appender);
+
+    let time_format =
+        format_description!("[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:6]Z");
+    let time_format = fmt::time::UtcTime::new(time_format);
+
+    let console_layer = fmt::layer()
+        .with_timer(time_format.clone())
+        .with_target(true)
+        .with_ansi(true) // color enabled for console
+        .with_level(true);
+
+    let file_layer = fmt::layer()
+        .with_timer(time_format)
+        .with_writer(non_blocking_appender)
+        .with_target(true)
+        .with_ansi(false) // color disabled for file
+        .with_level(true);
+
+    let env_filter = EnvFilter::new("debug");
+
+    tracing_subscriber::registry()
+        .with(env_filter)
+        .with(console_layer)
+        .with(file_layer)
+        .init();
+
+    Ok(guard)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-use crate::errors::InitLoggerError;
 use clap::Parser;
 
 use log::error;
 use qb_sdk;
 
 mod command;
-mod errors;
+mod logger;
 
 #[tokio::main]
 async fn main() -> Result<(), i32> {
-    match init_logger() {
-        Ok(_) => {}
+    let _guard = match logger::init_logger() {
+        Ok(guard) => guard,
         Err(e) => {
             error!("{}", e);
             return Err(1);
         }
-    }
+    };
     match run().await {
         Ok(_) => Ok(()),
         Err(e) => {
@@ -23,58 +22,6 @@ async fn main() -> Result<(), i32> {
             Err(1)
         }
     }
-}
-
-fn init_logger() -> Result<(), InitLoggerError> {
-    use fern::colors::Color;
-    use fern::colors::ColoredLevelConfig;
-    use tracing_appender::rolling::{RollingFileAppender, Rotation};
-
-    let colors = ColoredLevelConfig::new()
-        .error(Color::Red)
-        .warn(Color::Yellow)
-        .info(Color::Green)
-        .debug(Color::Blue)
-        .trace(Color::BrightBlack);
-
-    let console_dispatch = fern::Dispatch::new()
-        .format(move |out, message, record| {
-            out.finish(format_args!(
-                "[{timestamp} {level} {target}] {message}",
-                timestamp = chrono::Utc::now().format("%Y-%m-%dT%H:%M:%S%.3fZ"),
-                level = colors.color(record.level()),
-                target = record.target(),
-                message = message
-            ))
-        })
-        .chain(std::io::stdout());
-
-    let file_appender = RollingFileAppender::builder()
-        .rotation(Rotation::DAILY)
-        .filename_prefix("app")
-        .filename_suffix("log")
-        .max_log_files(7)
-        .build("logs")?;
-
-    let file_dispatch = fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "[{timestamp} {level} {target}] {message}",
-                timestamp = chrono::Utc::now().format("%Y-%m-%dT%H:%M:%S%.3fZ"),
-                level = record.level(),
-                target = record.target(),
-                message = message
-            ))
-        })
-        .chain(Box::new(file_appender) as Box<dyn std::io::Write + Send>);
-
-    fern::Dispatch::new()
-        .level(log::LevelFilter::Debug)
-        .chain(console_dispatch)
-        .chain(file_dispatch)
-        .apply()?;
-
-    Ok(())
 }
 
 async fn run() -> Result<(), String> {
