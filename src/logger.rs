90c09a89bfdba5757cfaf25f4cde5e5e3f890d3c
tsctsc6 <tsctsc6@126.com>
2026-03-31 19:19:50 +0800

    custom format

--- src/logger.rs
+++ src/logger.rs
@@ -1,7 +1,12 @@
 use thiserror::Error;
+use time::OffsetDateTime;
 use time::macros::format_description;
+use tracing::Event;
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
+use tracing_subscriber::fmt::format::Writer;
+use tracing_subscriber::fmt::{FmtContext, FormatEvent, FormatFields};
+use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Error, Debug)]
@@ -21,22 +26,15 @@ pub fn init_logger(verbose: u8) -> Result<WorkerGuard, Error> {
 
     let (non_blocking_appender, guard) = tracing_appender::non_blocking(file_appender);
 
-    let time_format =
-        format_description!("[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:6]Z");
-    let time_format = fmt::time::UtcTime::new(time_format);
-
-    let console_layer = fmt::layer()
-        .with_timer(time_format.clone())
-        .with_target(true)
-        .with_ansi(true) // color enabled for console
-        .with_level(true);
+    let console_layer = fmt::layer().event_format(MyCustomFormatter {
+        display_colors: true,
+    });
 
     let file_layer = fmt::layer()
-        .with_timer(time_format)
         .with_writer(non_blocking_appender)
-        .with_target(true)
-        .with_ansi(false) // color disabled for file
-        .with_level(true);
+        .event_format(MyCustomFormatter {
+            display_colors: false,
+        });
 
     let env_filter = EnvFilter::new(get_log_level(verbose));
 
@@ -57,5 +55,60 @@ fn get_log_level(verbose: u8) -> &'static str {
         3 => "info",
         4 => "debug",
         _ => "trace",
+    }
+}
+
+struct MyCustomFormatter {
+    display_colors: bool,
+}
+
+impl<S, N> FormatEvent<S, N> for MyCustomFormatter
+where
+    S: tracing::Subscriber + for<'a> LookupSpan<'a>,
+    N: for<'writer> FormatFields<'writer> + 'static,
+{
+    fn format_event(
+        &self,
+        ctx: &FmtContext<'_, S, N>,
+        mut writer: Writer<'_>,
+        event: &Event<'_>,
+    ) -> Result<(), std::fmt::Error> {
+        // time
+        let now = OffsetDateTime::now_utc();
+        let format = format_description!(
+            "[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:6]Z"
+        );
+        let time_str = now
+            .format(&format)
+            .unwrap_or_else(|_| "unknown time".to_string());
+
+        // metadata
+        let meta = event.metadata();
+        let level = meta.level();
+        let target = meta.target();
+
+        // header with optional colors
+        let reset_code = "\x1b[0m";
+        let gray_code = "\x1b[90m";
+        if self.display_colors {
+            let color_code = match *level {
+                tracing::Level::ERROR => "\x1b[31m", // red
+                tracing::Level::WARN => "\x1b[33m",  // yellow
+                tracing::Level::INFO => "\x1b[32m",  // green
+                tracing::Level::DEBUG => "\x1b[34m", // blue
+                tracing::Level::TRACE => "\x1b[90m", // gray
+            };
+            write!(
+                writer,
+                "[{gray_code}{time_str} {color_code}{level:>5} {gray_code}{target}{reset_code}] "
+            )?;
+        } else {
+            write!(writer, "[{time_str} {level:>5} {target}] ")?;
+        }
+
+        // message and fields
+        ctx.field_format().format_fields(writer.by_ref(), event)?;
+
+        writeln!(writer)
     }
 }
